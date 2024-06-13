dbc02de071e667f91c996eb2b40553396d6051b4
AndreyDov <dovban.andreyy@rambler.ru>
2024-06-13 21:10:36 +0300

    fix

--- script.js
+++ script.js
@@ -4,14 +4,25 @@ let int;
 but.onclick = () => {
 	if (!int) {
 		console.log('start');
-		int = setInterval(() => {
+		int = setInterval(async () => {
 			if (!f) {
 				f = true;
 				console.log('fetch');
-				fetch(url).then(v => {
-					console.log(v);
-					f = false;
-				});
+				try {
+					let v = await fetch(url);
+					if (v.status == 200) {
+						console.log(v);
+						f = false;
+					} else {
+						clearInterval(int);
+						int = null;
+						console.log('error ', v.status);
+					}
+				} catch (error) {
+					clearInterval(int);
+					int = null;
+					console.log(error.message);
+				}
 			} else {
 				console.log('skip');
 			}
