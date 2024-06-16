86478ee2e59889d3f40800dd5545cfadfa829ebc
AndreyDov <dovban.andreyy@rambler.ru>
2024-06-16 19:28:06 +0300

    add: завершон

--- script.js
+++ script.js
@@ -12,29 +12,31 @@ minv.innerText = down.value;
 maxv.innerText = up.value;
 max.innerText = down.max;
 
-let pers = (up.max - down.min) / 100;
+let p = (up.max - down.min) / 100;
 
-diapazon.style.marginLeft = +down.value + 1 + '%';
-diapazon.style.width = maxv.innerText - minv.innerText + '%';
+diapazon.style.marginLeft = (+down.value - +down.min) / p + 2 + '%';
+diapazon.style.width = (maxv.innerText - minv.innerText) / p - 2 + '%';
 
-down.oninput = e => {
-	let t = e.target;
-	minv.innerText = +t.value;
-	if (+t.value >= +up.value) {
-		up.value = +t.value;
-		maxv.innerText = +t.value;
+down.oninput = () => {
+	minv.innerText = +down.value;
+	if (+down.value >= +up.value) {
+		up.value = +down.value;
+		maxv.innerText = +down.value;
+		diapazon.style.width = 0;
+	} else {
+		diapazon.style.width = (+up.value - +down.value) / p - 2 + '%';
 	}
-	diapazon.style.marginLeft = +t.value + 1 + '%';
-	diapazon.style.width = maxv.innerText - minv.innerText + '%';
+	diapazon.style.marginLeft = (+down.value - +down.min) / p + 2 + '%';
 };
 
-up.oninput = e => {
-	let t = e.target;
-	maxv.innerText = +t.value;
-	if (+t.value <= +down.value) {
-		down.value = +t.value;
-		minv.innerText = +t.value;
+up.oninput = () => {
+	maxv.innerText = +up.value;
+	if (+up.value <= +down.value) {
+		down.value = +up.value;
+		minv.innerText = +up.value;
+		diapazon.style.width = 0;
+	} else {
+		diapazon.style.width = (+up.value - +down.value) / p - 2 + '%';
 	}
-	diapazon.style.marginLeft = +down.value + 1 + '%';
-	diapazon.style.width = maxv.innerText - minv.innerText + '%';
+	diapazon.style.marginLeft = (+down.value - +down.min) / p + 2 + '%';
 };
