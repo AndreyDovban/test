c8334f98195698c0757a73e26242aba6158bea6f
AndreyDov <dovban.andreyy@rambler.ru>
2024-07-24 04:37:10 +0300

    работа с indexDb

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-import { json2csv } from 'json-2-csv';
+// import { json2csv } from 'json-2-csv';
 const but = document.querySelector('.but');
-import f from './names.json';
+// import f from './names.json';
 
 but.onclick = () => console.log('work');
 
-let t = json2csv([f]);
+// let t = json2csv([f]);
 
-console.log(t);
+// console.log(t);
+
+if (!window.indexedDB) {
+	console.log('Ваш браузер не поддерживает стабильную версию IndexedDB. Такие-то функции будут недоступны');
+} else {
+	console.log('Всё работает!');
+}
+const request = indexedDB.open('library');
+let db;
+
+request.onsuccess = e => console.log(e.type);
+request.onerror = e => console.log(e.type);
+
+request.onupgradeneeded = e => {
+	db = e.target.result;
+	const store = db.createObjectStore('books', { keyPath: 'isbn' });
+	store.createIndex('by_title', 'title', { unique: true });
+	store.createIndex('by_author', 'author');
+
+	store.put({ title: 'Quarry Memories', author: 'Fred', isbn: 123456 });
+	store.put({ title: 'Water Buffaloes', author: 'Fred', isbn: 234567 });
+	store.put({ title: 'Bedrock Nights', author: 'Barney', isbn: 345678 });
+};
