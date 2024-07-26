e47aaaece66a4b66666e68d3d21d4f90e2fdc6b8
dovban <dovban@mdis.ru>
2024-07-26 13:19:19 +0300

    работа над csv

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 import { json2csv } from 'json-2-csv';
-const but = document.querySelector('.but');
 import f from './data.json';
+import fields from './data_26-07-2024-09-38-52.json';
 
-but.onclick = () => console.log('work');
+const but = document.querySelector('.but');
+const pre = document.querySelector('.pre');
 
-let t = json2csv([f]);
+but.onclick = func;
 
-console.log(t);
+function func() {
+	let t = json2csv([f]);
+
+	pre.innerText = t;
+
+	let date = new Date().toLocaleDateString('ru-Ru').replace(/\./g, '-');
+	let time = new Date().toLocaleTimeString('ru-Ru').replace(/:/g, '-');
+	let file = new File([t], `pdf_diff_report_${date}-${time}.csv`);
+
+	let link = document.createElement('a');
+	link.download = file.name;
+
+	link.href = URL.createObjectURL(file);
+	link.click();
+	URL.revokeObjectURL(link.href);
+}
+
+function func2() {
+	console.log(JSON.stringify(Object.keys(fields)));
+}
